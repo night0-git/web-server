1420c4d18738de15fa79904ffe51d7b04edd8b07
Nguyen Thanh Nhat <nguyenthanhnhat397@gmail.com>
2026-07-15 10:17:38 +0700

    Handle multiple clients (blocking)

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define BUF_SIZE 1024
 
@@ -29,34 +30,47 @@ int main() {
     }
 
     struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (client_sock == -1) {
-        perror("accept");
-        return 1;
-    } else {
-        printf("Client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        char buf[BUF_SIZE];
-
-        ssize_t bytes;
-        while ((bytes = read(client_sock, buf, sizeof(buf))) > 0) {
-            buf[bytes] = '\0';
-            printf("Message: %s", buf);
-
-            if (write(client_sock, buf, bytes) == -1) {
-                perror("write");
-                return 1;
-            }
-        }
-
-        if (bytes == -1) {
-            perror("read");
+    socklen_t client_addr_len;
+    int client_sock;
+    while (true) {
+        client_addr_len = sizeof(client_sock);
+        client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_sock == -1) {
+            perror("accept");
             return 1;
         }
-    }
+        printf("Client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-    close(client_sock);
-    close(listen_sock);
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            return 1;
+        }
+
+        if (pid == 0) {
+            // Child
+            close(listen_sock);
+
+            // Handle client
+            char buf[BUF_SIZE];
+            ssize_t bytes;
+            while ((bytes = read(client_sock, buf, sizeof(buf))) > 0) {
+                buf[bytes] = '\0';
+                printf("Message from %s:%s: %s", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf);
+
+                if (write(client_sock, buf, bytes) == -1) {
+                    perror("write");
+                    return 1;
+                }
+            }
+
+            close (client_sock);
+            _exit(0);
+        } else {
+            // Parent
+            close(client_sock);
+        }
+    }
 
     return 0;
 }
